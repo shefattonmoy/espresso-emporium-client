713f225cf3f5c77aa4fbe6daf32feadabb40a824
Shefat Tonmoy <shefathossain7@gmail.com>
2023-05-13 21:29:57 +0600

    added coffee

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-6xl text-purple-600'>Espresso Emporium</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/UpdateCoffee/UpdateCoffee.jsx
@@ -0,0 +1,9 @@
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h3>Coffee updated</h3>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
