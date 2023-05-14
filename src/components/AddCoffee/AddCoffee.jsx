db0de855f7c109784fc3613e55d2bfaf50e7ba31
Shefat Tonmoy <shefathossain7@gmail.com>
2023-05-14 18:47:43 +0600

    done

--- src/components/AddCoffee/AddCoffee.jsx
+++ src/components/AddCoffee/AddCoffee.jsx
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2';
+
 const AddCoffee = () => {
     const handleAddCoffee = event => {
         event.preventDefault();
@@ -10,12 +12,34 @@ const AddCoffee = () => {
         const details = form.details.value;
         const photo = form.photo.value;
 
-        const newCoffee = {name, quantity, supplier, taste, category, details, photo};
+        const newCoffee = { name, quantity, supplier, taste, category, details, photo };
         console.log(newCoffee);
+
+        // Send data to the server
+
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Coffee Added Successfully!',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
+                }
+            })
     }
     return (
         <div className="bg-[#F4F3F0] p-24">
-            <h3 className="text-3xl font-extrabold">Coffee Added</h3>
+            <h3 className="text-3xl font-extrabold">Add Coffee</h3>
             <form onSubmit={handleAddCoffee}>
                 {/* Form Name & Quantity Row */}
                 <div className="md:flex mb-8">
--- src/main.jsx
+++ src/main.jsx
@@ -13,14 +13,16 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    loader: () => fetch('http://localhost:5000/coffee')
   },
   {
     path: "/addcoffee",
     element: <AddCoffee></AddCoffee>
   },
   {
-    path: "/updatecoffee",
-    element: <UpdateCoffee></UpdateCoffee>
+    path: "/updatecoffee/:id",
+    element: <UpdateCoffee></UpdateCoffee>,
+    loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
   }
 ]);
 
